26a349f66946e0d96425bdcc105a7682d03b3a6f
Mohammad Biabani <biabani.mohammad@gmail.com>
2019-12-28 00:41:06 +0330

    init

---
+++ src/main/java/ir/mbbn/core/hibernate/BasicDataSource.java
@@ -0,0 +1,4 @@
+package ir.mbbn.core.hibernate;
+
+public class BasicDataSource extends org.apache.commons.dbcp2.BasicDataSource {
+}
---
+++ src/main/java/ir/mbbn/core/hibernate/PersistenceContext.java
@@ -0,0 +1,83 @@
+package ir.mbbn.core.hibernate;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+
+@Configuration
+@PropertySource("classpath*:server.properties")
+@EnableJpaRepositories("ir.mbbn.core.repositories")
+@EnableTransactionManagement
+public class PersistenceContext {
+
+    @Bean
+    JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+
+    @Bean
+    LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, Environment env) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactoryBean.setPackagesToScan("ir.mbbn.shopping.model");
+
+        Properties jpaProperties = new Properties();
+
+        //Configures the used database dialect. This allows Hibernate to create SQL
+        //that is optimized for the used database.
+        jpaProperties.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
+
+        //Specifies the action that is invoked to the database when the Hibernate
+        //SessionFactory is created or closed.
+        jpaProperties.put("hibernate.hbm2ddl.auto",
+                env.getRequiredProperty("hibernate.hbm2ddl.auto")
+        );
+
+        //Configures the naming strategy that is used when Hibernate creates
+        //new database objects and schema elements
+        jpaProperties.put("hibernate.ejb.naming_strategy",
+                env.getRequiredProperty("hibernate.ejb.naming_strategy")
+        );
+
+        //If the value of this property is true, Hibernate writes all SQL
+        //statements to the console.
+        jpaProperties.put("hibernate.show_sql",
+                env.getRequiredProperty("hibernate.show_sql")
+        );
+
+        //If the value of this property is true, Hibernate will format the SQL
+        //that is written to the console.
+        jpaProperties.put("hibernate.format_sql",
+                env.getRequiredProperty("hibernate.format_sql")
+        );
+
+        entityManagerFactoryBean.setJpaProperties(jpaProperties);
+
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    DataSource dataSource(Environment env){
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName(env.getRequiredProperty("db.driver"));
+        basicDataSource.setUrl(env.getRequiredProperty("db.url"));
+        basicDataSource.setUsername(env.getRequiredProperty("db.username"));
+        basicDataSource.setPassword(env.getRequiredProperty("db.password"));
+        return basicDataSource;
+    }
+
+}
