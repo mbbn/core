61939a1a0595bcc89c1e9a4c1f54a06ed65fc095
Mohammad Biabani <biabani.mohammad@gmail.com>
2020-01-25 03:20:45 +0330

    .

---
+++ src/main/java/ir/mbbn/shopping/utils/GenericConverter.java
@@ -0,0 +1,26 @@
+package ir.mbbn.shopping.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.toList;
+
+public interface GenericConverter<I, O> extends Function<I, O> {
+
+    default O convert(final I input) {
+        O output = null;
+        if (input != null) {
+            output = this.apply(input);
+        }
+        return output;
+    }
+
+    default List<O> convert(final List<I> input) {
+        List<O> output = new ArrayList<O>();
+        if (input != null) {
+            output = input.stream().map(this).collect(toList());
+        }
+        return output;
+    }
+}
